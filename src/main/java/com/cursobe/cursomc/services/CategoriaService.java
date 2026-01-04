746058c17126ec3ea752312bfd057a8de6d59a06
Betinsk <gmdust@hotmail.com>
2026-01-04 20:52:34 -0300

    Tratamento de excessões

---
+++ src/main/java/com/cursobe/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.cursobe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursobe.cursomc.domain.Categoria;
+import com.cursobe.cursomc.repositories.CategoriaRepository;
+import com.cursobe.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id> "+ id + ", Tipo: "+ 
+				Categoria.class.getName()));
+	}
+	
+}
